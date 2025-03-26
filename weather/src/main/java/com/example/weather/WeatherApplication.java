7769ffd96b43e8294989978889da03eeb53e45c9
abdelrahmansameh092@gmail.com <abdelrahmansameh092@gmail.com>
2025-03-26 22:10:11 +0200

    project initialization fix

---
+++ weather/src/main/java/com/example/weather/WeatherApplication.java
@@ -0,0 +1,29 @@
+package com.example.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
+
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class WeatherServerApplication {
+//     public static void main(String[] args) {
+//         // Start the Spring Boot application
+//         SpringApplication.run(WeatherServerApplication.class, args);
+//     }
+// }
+
+
+
+
